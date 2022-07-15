5cc20a12670f9582987cf34636eb116b7bae2518
KYUSEONGHAN <tyty587587@gmail.com>
2022-07-15 15:58:00 +0900

    2022.7.15.금: first commit: create spring project

---
+++ SpringNoticeBoard/src/main/java/com/example/springnoticeboard/SpringNoticeBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.springnoticeboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNoticeBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringNoticeBoardApplication.class, args);
+    }
+
+}
---
+++ SpringNoticeBoard/src/test/java/com/example/springnoticeboard/SpringNoticeBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springnoticeboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNoticeBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
